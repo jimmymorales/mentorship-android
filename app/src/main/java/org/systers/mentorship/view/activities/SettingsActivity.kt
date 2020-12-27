1cfcafa8b8c1b6c51896699867c294e5509d7289
Jimmy <me@jimmymorales.dev>
2020-12-27 10:30:46 -0600

    Remove Feedback button from settings

--- app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
+++ app/src/main/java/org/systers/mentorship/view/activities/SettingsActivity.kt
@@ -47,10 +47,6 @@ class SettingsActivity : BaseActivity() {
         tvAbout.setOnClickListener {
             startActivity(Intent(baseContext, AboutActivity::class.java))
         }
-
-        tvFeedback.setOnClickListener {
-            startActivity(Intent(baseContext, FeedbackActivity::class.java))
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
